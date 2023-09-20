d2bf4e2f8d1707663194f777b2d787df7aff9592
M1NO001 <130371068+M1NO001@users.noreply.github.com>
2023-09-20 20:08:02 +0900

    입출금 UI

---
+++ Assets/Scripts/UserInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo : MonoBehaviour
+{
+    public string UserName = "¹Ú¹ÎÈ£";
+    public int cash = 100000;
+    public int balance = 50000;
+}
